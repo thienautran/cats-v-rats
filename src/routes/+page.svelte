e13bb10257424f6981c2ec94295f63a63ae734b1
thienautran <thien.autran75@gmail.com>
2023-09-30 01:38:48 +1000

    chore: added tailwind

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	const value = 'hello, this is a reactive value';
+</script>
+
+<div
+	class="min-h-screen min-w-full bg-slate-800 text-white flex justify-center items-center flex-col"
+>
+	<h1>Welcome to SvelteKit</h1>
+	<p>
+		{value}
+	</p>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,3 +1,4 @@
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -5,9 +6,12 @@ const config = {
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
+		
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter()
-	}
+	},
+
+	preprocess: [vitePreprocess({})]
 };
 
 export default config;
