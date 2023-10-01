fae3b11e37c579119b51230d9fa2f68717b57394
thienautran <thien.autran75@gmail.com>
2023-10-02 01:34:52 +1100

    refactor: moved starting form to own component and called in main page

---
+++ src/lib/components/PlayerInfoForm.svelte
@@ -0,0 +1,31 @@
+<script>
+	let player1;
+	let player2;
+
+	const handleSubmitForm = () => {
+		console.log(player1, player2);
+	};
+</script>
+
+<div class="flex flex-col space-y-4 text-xl justify-center items-center text-center">
+	<h1 class="text-4xl">Welcome to the game!</h1>
+
+	<p>Before we get started, please enter some player info below</p>
+
+	<form
+		class="flex flex-col justify-center items-center space-y-2 mt-10"
+		on:submit|preventDefault={handleSubmitForm}
+	>
+		<label>
+			Player 1:
+			<input type="text" placeholder="Enter a name for Player 1: " bind:value={player1} />
+		</label>
+
+		<label>
+			Player 2:
+			<input type="text" placeholder="Enter a name for Player 2: " bind:value={player2} />
+		</label>
+
+		<button class="bg-sky-600 text-2xl rounded-lg py-2 px-6 mt-20">Start game</button>
+	</form>
+</div>
